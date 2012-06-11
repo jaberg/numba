ecd0d85603ee6fef9fafc90e294bcc6195bb462b
James Bergstra <james.bergstra@gmail.com>
2012-06-11 11:34:22 -0400

    linear_svm example works again

--- examples/linear_svm.py
+++ examples/linear_svm.py
@@ -14,27 +14,25 @@ import sys
 import numpy as np
 from numba.ad import fmin
 
-
-# a Python function with no data-dapendent control flow in the bytecode
-#
-# (A lot of mathematical programming fits into this limited Python subset.)
 def binary_svm_hinge_loss(weights, bias, x, y, l2_regularization):
+    # print x, y
     margin = y * (np.dot(x, weights) + bias)
     loss = np.maximum(0, 1 - margin) ** 2
     l2_cost = 0.5 * l2_regularization * np.dot(weights, weights)
-    # we can still convert this to single-assignment form easily
     loss = np.mean(loss) + l2_cost
     return loss
 
 
 def main():
-    # SVM-specific hack here
-    loss_fn = functools.partial(binary_svm_hinge_loss,
-            x=np.random.rand(10, 7),
-            y=2 * (np.random.rand(10) > 0.5) - 1,
-            l2_regularization=0.00001)
 
-    w, b = fmin(loss_fn, [np.zeros(X.shape[1]), np.zeros(())])
+    # create some fake data
+    x = np.random.rand(10, 7)
+    y = 2 * (np.random.rand(10) > 0.5) - 1
+
+    def loss_fn(w, b):
+        return binary_svm_hinge_loss(w, b, x, y, 1e-4)
+
+    w, b = fmin(loss_fn, [np.zeros(7), np.zeros(())])
 
     print 'Best-fit SVM:'
     print ' -> cost:', loss_fn(w, b)
--- numba/ad.py
+++ numba/ad.py
@@ -19,6 +19,8 @@ import theano
 
 from .utils import itercode
 
+from scipy.optimize.lbfgsb import fmin_l_bfgs_b
+
 # Opcode help: http://docs.python.org/library/dis.html
 
 # XXX: support full calling convention for named args, *args and **kwargs
@@ -33,7 +35,7 @@ class FrameVM(object):
 
     """
     def __init__(self, watcher, func):
-        print 'FrameVM', func
+        #print 'FrameVM', func
         self.watcher = watcher
         self.func = func
         self.fco = func.func_code
@@ -71,7 +73,7 @@ class FrameVM(object):
             except StopIteration:
                 break
             name = opcode.opname[op]
-            #print 'OP: ', i, name
+            # print 'OP: ', i, name
             jmp = getattr(self, 'op_' + name)(i, op, arg)
 
         return self.rval
@@ -79,36 +81,42 @@ class FrameVM(object):
     def op_BINARY_ADD(self, i, op, arg):
         arg2 = self.stack.pop(-1)
         arg1 = self.stack.pop(-1)
+        assert not hasattr(arg1, 'type')
+        assert not hasattr(arg2, 'type')
         r = arg1 + arg2
         self.stack.append(r)
         if (id(arg1) in self.watcher.svars 
                 or id(arg2) in self.watcher.svars):
             s1 = self.watcher.svars.get(id(arg1), arg1)
             s2 = self.watcher.svars.get(id(arg2), arg2)
-            self.watcher.svars[id(r)] = s1 + s2
+            self.watcher.shadow(r, s1 + s2)
             #print 'added sym'
 
     def op_BINARY_SUBTRACT(self, i, op, arg):
         arg2 = self.stack.pop(-1)
         arg1 = self.stack.pop(-1)
+        assert not hasattr(arg1, 'type')
+        assert not hasattr(arg2, 'type')
         r = arg1 - arg2
         self.stack.append(r)
         if (id(arg1) in self.watcher.svars 
                 or id(arg2) in self.watcher.svars):
             s1 = self.watcher.svars.get(id(arg1), arg1)
             s2 = self.watcher.svars.get(id(arg2), arg2)
-            self.watcher.svars[id(r)] = s1 - s2
+            self.watcher.shadow(r,  s1 - s2)
 
     def op_BINARY_MULTIPLY(self, i, op, arg):
         arg2 = self.stack.pop(-1)
         arg1 = self.stack.pop(-1)
         r = arg1 * arg2
         self.stack.append(r)
+        assert not hasattr(arg1, 'type')
+        assert not hasattr(arg2, 'type')
         if (id(arg1) in self.watcher.svars 
                 or id(arg2) in self.watcher.svars):
             s1 = self.watcher.svars.get(id(arg1), arg1)
             s2 = self.watcher.svars.get(id(arg2), arg2)
-            self.watcher.svars[id(r)] = s1 * s2
+            self.watcher.shadow(r, s1 * s2)
             #print 'mul sym', id(r)
 
     def op_BINARY_POWER(self, i, op, arg):
@@ -120,11 +128,12 @@ class FrameVM(object):
                 or id(arg2) in self.watcher.svars):
             s1 = self.watcher.svars.get(id(arg1), arg1)
             s2 = self.watcher.svars.get(id(arg2), arg2)
-            self.watcher.svars[id(r)] = s1 ** s2
+            self.watcher.shadow(r, s1 ** s2)
             #print 'mul sym', id(r)
 
     def op_CALL_FUNCTION(self, i, op, arg):
         # XXX: does this work with kwargs?
+        # print 'CALL_FUNCTION', arg
         args = [self.stack[-ii] for ii in range(arg, 0, -1)]
         if arg > 0:
             self.stack = self.stack[:-arg]
@@ -141,35 +150,49 @@ class FrameVM(object):
             recurse = False
 
         if recurse:
+            # print 'stepping into', func
             vm = FrameVM(self.watcher, func)
             rval = vm.call(args, {})
         else:
-            #print 'running built-in', func, func.__name__, args
+            # print 'running built-in', func, func.__name__, args
             rval = func(*args)
             if any(id(a) in self.watcher.svars for a in args):
                 sargs = [self.watcher.svars.get(id(a), a)
                         for a in args]
                 if func.__name__ == 'sum':
-                    self.watcher.svars[id(rval)] = theano.tensor.sum(*sargs)
+                    self.watcher.shadow(rval, theano.tensor.sum(*sargs))
                 elif func.__name__ == 'dot':
-                    self.watcher.svars[id(rval)] = theano.tensor.dot(*sargs)
+                    self.watcher.shadow(rval, theano.tensor.dot(*sargs))
                 elif func.__name__ == 'mean':
-                    self.watcher.svars[id(rval)] = theano.tensor.mean(*sargs)
+                    self.watcher.shadow(rval, theano.tensor.mean(*sargs))
                 elif func.__name__ == 'maximum':
-                    self.watcher.svars[id(rval)] = theano.tensor.maximum(*sargs)
+                    self.watcher.shadow(rval, theano.tensor.maximum(*sargs))
                 else:
                     raise NotImplementedError(func)
         self.stack.append(rval)
 
     def op_COMPARE_OP(self, i, op, arg):
         opname = opcode.cmp_op[arg]
-        left = self.stack.pop(-1)
         right = self.stack.pop(-1)
+        left = self.stack.pop(-1)
         if 0: pass
         elif opname == '==': self.stack.append(left == right)
         elif opname == '!=': self.stack.append(left != right)
+        elif opname == '>': self.stack.append(left > right)
+        elif opname == '<': self.stack.append(left < right)
         else:
             raise NotImplementedError('comparison: %s' % opname)
+
+        if any(id(a) in self.watcher.svars for a in [left, right]):
+            sargs = [self.watcher.svars.get(id(a), a) for a in [left, right]]
+            tos = self.stack[-1]
+            if 0: pass
+            elif opname == '<':
+                self.watcher.shadow(tos, theano.tensor.lt(left, right))
+            elif opname == '>':
+                self.watcher.shadow(tos, theano.tensor.gt(left, right))
+            else:
+                raise NotImplementedError()
 
     def op_FOR_ITER(self, i, op, arg):
         # either push tos.next()
@@ -177,14 +200,14 @@ class FrameVM(object):
         tos = self.stack[-1]
         try:
             next = tos.next()
-            print 'next', next
+            # print 'next', next
             self.stack.append(next)
         except StopIteration:
             self.stack.pop(-1)
             return ('rel', arg)
 
     def op_JUMP_ABSOLUTE(self, i, op, arg):
-        print 'sending', arg
+        # print 'sending', arg
         return ('abs', arg)
 
     def op_JUMP_IF_TRUE(self, i, op, arg):
@@ -200,7 +223,7 @@ class FrameVM(object):
             raise NotImplementedError('iterator of watched value')
 
     def op_LOAD_GLOBAL(self, i, op, arg):
-        #print 'LOAD_GLOBAL', self.names[arg]
+        # print 'LOAD_GLOBAL', self.names[arg]
         tos = self._myglobals[self.names[arg]]
         self.stack.append(tos)
         if (isinstance(tos, np.ndarray)
@@ -208,7 +231,7 @@ class FrameVM(object):
             raise NotImplementedError()
 
     def op_LOAD_ATTR(self, i, op, arg):
-        #print 'LOAD_ATTR', self.names[arg]
+        # print 'LOAD_ATTR', self.names[arg]
         TOS = self.stack[-1]
         self.stack[-1] = getattr(TOS, self.names[arg])
         tos = self.stack[-1]
@@ -217,15 +240,22 @@ class FrameVM(object):
             raise NotImplementedError()
 
     def op_LOAD_CONST(self, i, op, arg):
-        #print 'LOAD_CONST', self.constants[arg]
+        # print 'LOAD_CONST', self.constants[arg]
         self.stack.append(self.constants[arg])
         tos = self.stack[-1]
         if (isinstance(tos, np.ndarray)
                 and id(tos) not in self.watcher.svars):
             raise NotImplementedError()
 
+    def op_LOAD_DEREF(self, i, op, arg):
+        # print '???', i, op, arg
+        # print self.func.func_closure
+        thing = self.func.func_closure[arg]
+        # print dir(thing.cell_contents)
+        self.stack.append(thing.cell_contents)
+
     def op_LOAD_FAST(self, i, op, arg):
-        #print 'LOAD_FAST', self.varnames[arg]
+        # print 'LOAD_FAST', self.varnames[arg], self._locals[arg]
         tos = self._locals[arg]
         self.stack.append(tos)
         if (isinstance(tos, np.ndarray)
@@ -235,10 +265,11 @@ class FrameVM(object):
                 s_tos = theano.shared(tos.astype('uint8'), borrow=False)
             else:
                 s_tos = theano.shared(tos, borrow=False)
-            self.watcher.svars[id(tos)] = s_tos
+            self.watcher.shadow(tos, s_tos)
 
     def op_POP_BLOCK(self, i, op, arg):
-        print 'pop block, what to do?'
+        #print 'pop block, what to do?'
+        pass
 
     def op_POP_TOP(self, i, op, arg):
         self.stack.pop(-1)
@@ -250,10 +281,11 @@ class FrameVM(object):
         print ''
 
     def op_SETUP_LOOP(self, i, op, arg):
-        print 'SETUP_LOOP, what to do?'
+        #print 'SETUP_LOOP, what to do?'
+        pass
 
     def op_STORE_FAST(self, i, op, arg):
-        #print 'STORE_FAST', self.varnames[arg]
+        #print 'STORE_FAST', self.varnames[arg], self.stack[-1]
         self._locals[arg] = self.stack.pop(-1)
 
     def op_RAISE_VARARGS(self, i, op, arg):
@@ -274,6 +306,11 @@ class FrameVM(object):
 class Context(object):
     def __init__(self):
         self.svars = {}
+        self.nogc = [] # ids that must not be reused
+
+    def shadow(self, rval, sval):
+        self.svars[id(rval)] = sval
+        self.nogc.append(rval)
 
     def call(self, fn, args=(), kwargs={}):
         vm = FrameVM(self, fn)
@@ -289,56 +326,6 @@ class Context(object):
         sy = self.svars[id(rval)]
         sx = self.svars[id(ival)]
         return theano.function([sx], sy)
-
-    def fmin(self, cost, wrt, algo):
-        """
-        cost: a scalar that is known to this context e.g. by being a return
-              value of a previous self.call
-
-        wrt: a list of numpy ndarrays from which the `cost` was computed
-
-        algo: choose the optimization algorithm with (fmin, kwargs) tuple.
-            In future: require cost to be differentiable with respect to all
-            elements of `wrt` and optimize using fmin_l_bfgs_b
-
-        """
-
-        orig_s_wrt = [self.svars[id(w)] for w in wrt]
-        wrt_shapes = [w.shape for w in wrt]
-        wrt_sizes = [w.size for w in wrt]
-        x_size = sum(wrt_sizes)
-        x = np.empty(x_size)
-        s_x = theano.tensor.vector(dtype=x.dtype)
-        s_wrt = []
-        i = 0
-        for w in wrt:
-            x[i: i + w.size] = w.flatten()
-            if w.shape:
-                s_wrt.append(s_x[i: i + w.size].reshape(w.shape))
-            else:
-                s_wrt.append(s_x[i])
-            i += w.size
-
-        orig_s_cost = self.svars[id(cost)]
-        memo = theano.gof.graph.clone_get_equiv(
-                theano.gof.graph.inputs([orig_s_cost]),
-                [orig_s_cost],
-                memo=dict(zip(orig_s_wrt, s_wrt)))
-        s_cost = memo[orig_s_cost]
-
-        g_x = theano.tensor.grad(s_cost, s_x)
-
-        f_df = theano.function([s_x], [s_cost, g_x])
-
-        fmin, fmin_kwargs = algo
-        x_opt, mincost, info_dct = fmin(f_df, x, **fmin_kwargs)
-
-        rval = []
-        i = 0
-        for w in wrt:
-            rval.append(x_opt[i: i + w.size].reshape(w.shape))
-            i += w.size
-        return rval, mincost, info_dct
 
 
 def gradient(fn, args_like=None):
@@ -366,23 +353,71 @@ def gradient(fn, args_like=None):
 
     # unpack x_opt -> args-like quantity `args_opt`
 
-def fmin(fn, args):
-    raise NotImplementedError()
-    # args must be all float or np.ndarray
+def fmin(fn, args, algo=(fmin_l_bfgs_b, {})):
+    """
+    fn: a scalar-valued function of floats and float/complex arguments
 
-    # inspect bytecode of fn to determine derivative wrt args
+    args: a list of floats / complex / ndarrays from from which to start
+        optimizing `fn(*args)`
+
+    algo: choose the optimization algorithm with (fmin, kwargs) tuple.
+        In future: require cost to be differentiable with respect to all
+        elements of `wrt` and optimize using fmin_l_bfgs_b
+
+    """
+
+    # STEP 1: inspect bytecode of fn to determine derivative wrt args
+
+    # hacky way to get call graph (we could do it without actually running it)
+    ctxt = Context()
+    cost = ctxt.call(fn, args)
+
 
     # construct bytecode for f_df() that
     # * unpacks x-> args
     # * computes f, dx
+    orig_s_args = [ctxt.svars[id(w)] for w in args]
+    args_shapes = [w.shape for w in args]
+    args_sizes = [w.size for w in args]
+    x_size = sum(args_sizes)
+    x = np.empty(x_size)
+    s_x = theano.tensor.vector(dtype=x.dtype)
+    s_args = []
+    i = 0
+    for w in args:
+        x[i: i + w.size] = w.flatten()
+        if w.shape:
+            s_args.append(s_x[i: i + w.size].reshape(w.shape))
+        else:
+            s_args.append(s_x[i])
+        i += w.size
+
+    orig_s_cost = ctxt.svars[id(cost)]
+    memo = theano.gof.graph.clone_get_equiv(
+            theano.gof.graph.inputs([orig_s_cost]),
+            [orig_s_cost],
+            memo=dict(zip(orig_s_args, s_args)))
+    s_cost = memo[orig_s_cost]
+    g_x = theano.tensor.grad(s_cost, s_x)
+
 
     # [optional] pass bytecode for g() to numba.translate to compile a faster
     # implementation for the repeated calls that are coming up
 
+    # XXX: hacky current thing does not pass by a proper byte-code optimizer
+    # because numba isn't quite there yet. For now we just compile the call
+    # graph we already built theano-style.
+    f_df = theano.function([s_x], [s_cost, g_x])
+
     # pass control to iterative minimizer
-    x_opt, mincost, info_dct = fmin_l_bfgs_b(f_df, x, **fmin_kwargs)
+    #x_opt, mincost, info_dct = fmin_l_bfgs_b(f_df, x, **fmin_kwargs)
+    fmin, fmin_kwargs = algo
+    x_opt, mincost, info_dct = fmin(f_df, x, **fmin_kwargs)
 
     # unpack x_opt -> args-like quantity `args_opt`
-
-    return args_opt
-
+    rval = []
+    i = 0
+    for w in args:
+        rval.append(x_opt[i: i + w.size].reshape(w.shape))
+        i += w.size
+    return rval #, mincost, info_dct
